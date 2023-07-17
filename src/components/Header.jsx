95a3e617795c71b0e0f132fa0aee7241ce082c49
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-17 21:08:00 +0300

    exmp. nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,23 @@ import Nav from "./styled/Nav";
 const Header = () => {
   return (
     <div>
-        <Nav>
-            <div>
-                <img src="./images/logo.png" alt="logo" width={"300px"} />
-            </div>
-        </Nav>
-     <div>
-     <Button color="#AF3A53">Apply Courses</Button> 
-      <Button bg="#AF3A53">Talk to Advisor</Button> 
-     </div>
-   
+      <Nav>
+        <div>
+          <img src="./images/logo.png" alt="logo" width={"300px"} />
+        </div>
+      </Nav>
+      <div>
+        <Button color="#AF3A53">Apply Courses</Button>
+        <Button bg="#AF3A53">Talk to Advisor</Button>
+      </div>
+      <div>
+        <h1>The IT Career of Your Dreams Starts Here!</h1>
+        <p>
+          Clarusway is a leading international software Bootcamp. Join a micro
+          class online with other trainees and learn coding skills with a
+          highly-skilled instructor.
+        </p>
+      </div>
     </div>
   );
 };
---
+++ src/components/styled/Flex.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Flex = styled.div`
+  
+`;
+
+export default Flex;
