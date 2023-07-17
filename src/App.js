d95edfc8d43cc0e88ce3e88e5e61c624ef0c13c1
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-17 22:33:38 +0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import {ThemeProvider} from "styled-components";
 import Header from "./components/Header";
+import Container from "./components/styled/Container";
+import { GlobalStyles } from "./components/styled/GlobalStyles";
 
 const App = () => {
+
+  const styles ={
+colors : {
+  primary:"#666",  
+  secondary:"#8A1C4A"
+},
+margins:{},
+paddings:{},
+breakpoints:{xs:"300px", sm:"500px", md:"700px"}
+
+
+
+  }
   return (
     <>
-      <Header/>
+      <ThemeProvider theme={styles}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
--- src/components/styled/Nav.jsx
+++ src/components/styled/Nav.jsx
@@ -6,6 +6,14 @@ justify-content: space-between;
 align-items: center;
 padding: 1rem;
 
+background-color: ${({theme}) => theme.colors.primary};
+
+
+
+
+@media (max-width:${({theme})=> theme.breakpoints.md}){
+    text-align: center;
+}
 `;
 
 export default Nav;
